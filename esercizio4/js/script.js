7f086c90c97ecf3b90a8c71f964c13304de1ed64
Marco Tessitore <marcotexcode@gmail.com>
2021-06-10 15:59:54 +0200

    add esercizio4 non compreso

--- esercizio4/js/script.js
+++ esercizio4/js/script.js
@@ -6,3 +6,34 @@
 
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
 //  La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+
+// Creare un array con x numeri
+var arr = [1,2,3,4,5,6,7,8,9];
+
+
+
+
+
+// creare due numeri variabili che restituiscono numeri  a piu grande di b;
+    var a = 2;
+    var b = 5;
+
+
+
+// creare un  ciclo che si ferma alla variabile piu grande 
+for(var i = 0; i < b; i++){
+
+    var arrNum  = [];
+
+    if (arr.length === a) {}
+
+
+    arrNum.push(arr.length === a);
+    arrNum.push(arr.length === b);
+
+}
+
+console.log(arrNum );
+
+
+
